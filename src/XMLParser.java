434c75b48fdaf4c867d0b22258b23943fa31b693
Milan Schuringa <milanschuringa@icloud.com>
2021-01-26 17:36:45 +0100

    updated Parser

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -2,20 +2,24 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 
 public class XMLParser {
 
-    public static void main(String[] args) {
-
+    public static void parse() throws ParserConfigurationException, IOException, SAXException {
         try {
-            File inputFile = new File("./UnwdmiGenerator/output.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            //factory.setValidating(true);
+            factory.setIgnoringElementContentWhitespace(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            File file = new File("./UnwdmiGenerator/output.xml");
+            Document doc = builder.parse(file);
             doc.getDocumentElement().normalize();
             System.out.println("Root element : " + doc.getDocumentElement().getNodeName());
             NodeList nList = doc.getElementsByTagName("MEASUREMENT");
@@ -99,8 +103,12 @@ public class XMLParser {
                             .getTextContent() + " degrees");
                 }
             }
-        } catch (Exception e) {
+        } catch (ParserConfigurationException | IOException | SAXException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+        XMLParser.parse();
     }
 }
